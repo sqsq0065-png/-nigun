df324e5130613cbb9fbab8e51ec1171935d4c694
sqsq0065-png <sqsq0065@gmail.com>
2026-09-08 10:38:04 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='nigun-supabase-v2',A=['./','index.html','app.js','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
